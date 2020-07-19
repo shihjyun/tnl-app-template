fc11fe4c6114ef9e49695dbc432cc192a74f899a
shihjyun <shihjyun.yeo@gmail.com>
2020-07-19 23:52:37 +0800

    initial commit - integrate tailwind css with svelte basic template(push)

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<main>
+	<h1>Hello TNL Infographics </h1>
+</main>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.getElementById("tnl-interactive-article-content")
+});
+
+export default app;
